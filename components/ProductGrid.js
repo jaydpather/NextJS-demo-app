292a930137edc2ea6ee95b2cb41ad76cfb0d7a63
Jayd <jaydpather@gmail.com>
2019-12-27 07:26:57 +0100

    product service passed to component as props

--- components/ProductGrid.js
+++ components/ProductGrid.js
@@ -8,31 +8,12 @@ import Product from "./Product";
 //      * list of products should be passed in as props
 //  * where do we call the getProducts() function?
 //      * need some place on the client side that isn't inside of a component
-function getProducts(){
-    return[
-        [{displayId: "MW0MW01234"}, {displayId: "MW0MW01235"}, {displayId: "MW0MW01236"}, ],
-        [{displayId: "MW0MW02234"}, {displayId: "MW0MW02235"}, {displayId: "MW0MW02236"}, ],
-        [{displayId: "MW0MW03234"}, {displayId: "MW0MW03235"}, {displayId: "MW0MW03236"}, ],
-        [{displayId: "MW0MW04234"}, {displayId: "MW0MW04235"}, {displayId: "MW0MW04236"}, ],
-    ];
-}
-
-function createArray(length){
-    var retVal = [];
-
-    for(var i=0; i<length; i++){
-        retVal[i] = i;
-    }
-
-    return retVal;
-}
-
 
 export default function ProductGrid(props){
     return(
         <table>
             <tbody>
-                {getProducts().map(row => 
+                {props.ProductService.getProducts().map(row => 
                 <tr>
                     {row.map(product => 
                     <td>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import { Route, Switch } from "react-router-dom";
 
+import ProductService from '../logic/productService'
+
 import PurchaseComplete from '../components/PurchaseComplete'
 import ProductGrid from "../components/ProductGrid";
 
@@ -25,6 +27,19 @@ function NotFound() {
 }
 
 function App() {
+  //todo:
+  //  * instantiate business layer
+  //    * instatiate data layer
+  //  * pass business layer to Components as a prop
+
+  /*
+  spyOn(foo, "getBar").and.callFake(function(arguments, can, be, received) {
+    return 1001;
+  });
+  */
+
+  var productService = new ProductService();
+
   return(
     <div>
       <h1>Welcome to my online store!</h1>
@@ -32,7 +47,7 @@ function App() {
       </nav>
 
       <Switch>
-        <Route path="/" exact render={(props) => <ProductGrid {...props} />} />
+        <Route path="/" exact render={(props) => <ProductGrid {...props} ProductService={productService} />} />
         <Route path="/purchaseComplete/" component={PurchaseComplete} />
         <Route component={NotFound}/>
       </Switch>
